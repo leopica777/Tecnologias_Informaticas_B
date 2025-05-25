fbabd14549868117c8b6754a3190a03c884d3c86
leopica <85765754+leopica777@users.noreply.github.com>
2025-05-25 20:03:07 -0300

    Update databaseConfig.php

--- CRUD REFACTORIZAR 2/backend/config/databaseConfig.php
+++ CRUD REFACTORIZAR 2/backend/config/databaseConfig.php
@@ -1,13 +1,17 @@
 <?php
+//CONFIG.PHP
+/*Configura y estable una conexion con la BD MYSQL usando la extension MYSQLI*/
+
 $host = "localhost";
 $user = "students_user";
 $password = "12345";
 $database = "students_db";
 
+/*Crea un nuevo objeto SQLI y establece la conexion usando las credenciales anteriores*/
 $conn = new mysqli($host, $user, $password, $database);
 
-if ($conn->connect_error) {
-    http_response_code(500);
-    die(json_encode(["error" => "Database connection failed"]));
+if ($conn->connect_error) { //Verifica si hubo algun error en la conexion
+    http_response_code(500); //Si falla responde con un codigo HTTP
+    die(json_encode(["error" => "Database connection failed"])); //Envia un mensaje JSON con clave error y detiene la ejecucion del script inmediatamente
 }
 ?>
