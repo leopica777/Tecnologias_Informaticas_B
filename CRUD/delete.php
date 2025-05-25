484e8ca2f2275fce5a4b90f85e2303c305cea0bb
leopica <85765754+leopica777@users.noreply.github.com>
2025-05-25 18:56:18 -0300

    Update delete.php

--- CRUD/delete.php
+++ CRUD/delete.php
@@ -1,14 +1,17 @@
 <?php
-include 'config.php';
+/*DELETE.PHP
+Permite eliminar un registro de la BD
+*/
+include 'config.php'; //INCLUYE CONFIG.PHP
 
-$id = $_GET['id'];
+$id = $_GET['id']; //OBTIENE EL VALOR DE LA ID QUE SE PASO POR LA URL POR METODO GET
 
-$sql = "DELETE FROM students WHERE id = $id";
+$sql = "DELETE FROM students WHERE id = $id"; //CREA SENTENCIA SQL DELETE PARA BORRAR A UN ESTUDIANTE POR ID
 
-if ($connection->query($sql) === TRUE) {
-    header("Location: index.php");
+if ($connection->query($sql) === TRUE) { //CONSULTA SI FUE EXITOSA LA CONSULTA
+    header("Location: index.php"); //REDIRIGE AL INDEX
     exit;
 } else {
-    echo "Error al borrar: " . $connection->error;
+    echo "Error al borrar: " . $connection->error; //SI HAY ERROR MUESTRA MENSAJE EN PANTALLA
 }
 ?>
