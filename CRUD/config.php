eb95ee2b0bd1069a23c03ad7176c65df1ccb885a
leopica <85765754+leopica777@users.noreply.github.com>
2025-05-25 18:52:40 -0300

    Update config.php

--- CRUD/config.php
+++ CRUD/config.php
@@ -5,7 +5,7 @@
 /**
  * Datos de conexión: en variables en php.
  */
-$host = "localhost";
+$host = "localhost"; //VARIABLES QUE GUARDAN LAS CREDENCIALES
 $user = "students_user";
 $password = "12345";
 $database = "students_db";
@@ -14,13 +14,13 @@ $database = "students_db";
  * $connection "variable", objeto instancia de mysqli
  Crea un nuevo objeto mysqli y establece la conexión usando las credenciales anteriores.
  */
-$connection = new mysqli($host, $user, $password, $database);
+$connection = new mysqli($host, $user, $password, $database); //CREA UN NUEVO OBJETO DE CLASE MSQLI QUE REPRESENTA LA CONEXION A LA BD
 
 /**
  * Verificar conexión, si hay error corto interpretación con la función "die"
  * y muestro por pantalla el error. "Modo Desarrollo"
  */ 
-if ($connection->connect_error) {
-    die("Error de conexión: " . $connection->connect_error);
+if ($connection->connect_error) { //VERIFICA ERRORES 
+    die("Error de conexión: " . $connection->connect_error); //CORTA EL SCRIPT INMEDIATAMENTE
 }
 ?>
