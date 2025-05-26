d2f72f4df0d5a6daa4f20012e7de238a8cb85082
leopica <85765754+leopica777@users.noreply.github.com>
2025-05-25 21:17:55 -0300

    Update server.php

--- CRUD REFACTORIZAR 2/backend/server.php
+++ CRUD REFACTORIZAR 2/backend/server.php
@@ -1,18 +1,22 @@
 <?php
+//server.php
+//Punto de entrada principal del backend
+//separacion de responsabilidades, configura y delega
+
 /**
- * DEBUG MODE
+ * DEBUG MODE (SOLO EN MODO DESAROLLO)
  */
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 1); //Muestra errores directamente en pantalla
+error_reporting(E_ALL); //Muestra todos los errores posibles
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Origin: *"); //Permite que cualquier frontend pueda acceder al backend.
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); //Indica al navegador que metodos HTTP acepta el backend
+header("Access-Control-Allow-Headers: Content-Type");//Permite que el navegador envie encabezados personalizados
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { //en caso de un metodo options
+    http_response_code(200); //manda un mensaje de ok, pero no hace nada
     exit();
 }
 
-require_once("./routes/studentsRoutes.php");
+require_once("./routes/studentsRoutes.php"); //incluye las rutas
 ?>
