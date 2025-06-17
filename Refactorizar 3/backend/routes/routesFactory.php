f42ebdb9b83dee27d7c4e9d0c287f1ccacefbfd1
leopica <85765754+leopica777@users.noreply.github.com>
2025-06-17 20:58:07 -0300

    Update routesFactory.php

--- Refactorizar 3/backend/routes/routesFactory.php
+++ Refactorizar 3/backend/routes/routesFactory.php
@@ -9,21 +9,28 @@
 *    Iteration   : 3.0 ( prototype )
 */
 
-function routeRequest($conn, $customHandlers = [], $prefix = 'handle') 
-{
-    $method = $_SERVER['REQUEST_METHOD'];
+/*Este archivo define una función reutilizable llamada
+routeRequest(...) que se encarga de gestionar las rutas REST
+(GET, POST, PUT, DELETE) y de conectar cada método HTTP con la
+función que debe ejecutarse.*/
 
-    // Lista de handlers CRUD por defecto
+
+function routeRequest($conn, $customHandlers = [], $prefix = 'handle') //Funcion routeRequest. $customhandlers array de funciones personalizadas para ciertos metodos http. $prefix prefijo para construir los nombres por defecto de los handlers
+{
+    $method = $_SERVER['REQUEST_METHOD'];//Detecta el metodo HTTP de la peticion
+
+    // Lista de handlers CRUD por defecto. Define funciones por defecto
     $defaultHandlers = [
         'GET'    => $prefix . 'Get',
         'POST'   => $prefix . 'Post',
         'PUT'    => $prefix . 'Put',
         'DELETE' => $prefix . 'Delete'
-    ];
+    ]; //HandleGet. HandlePost. Handleput. HandleDelete
 
     // Sobrescribir handlers por defecto si hay personalizados
     $handlers = array_merge($defaultHandlers, $customHandlers);
 
+    //Valida si el metodo es soportado
     if (!isset($handlers[$method])) 
     {
         http_response_code(405);
@@ -31,7 +38,8 @@ function routeRequest($conn, $customHandlers = [], $prefix = 'handle')
         return;
     }
 
-    $handler = $handlers[$method];
+    //Ejecuta handler correspondiente
+    $handler = $handlers[$method];//guarda el nombre de la funcion
 
     if (is_callable($handler)) //si puede ser llamado como funcion
     {
