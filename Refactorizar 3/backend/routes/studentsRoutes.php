9e7a04a60880994e6eda7722baff6504121c68cd
leopica <85765754+leopica777@users.noreply.github.com>
2025-06-17 20:37:16 -0300

    Update studentsRoutes.php

--- Refactorizar 3/backend/routes/studentsRoutes.php
+++ Refactorizar 3/backend/routes/studentsRoutes.php
@@ -9,9 +9,15 @@
 *    Iteration   : 3.0 ( prototype )
 */
 
-require_once("./config/databaseConfig.php");
-require_once("./routes/routesFactory.php");
-require_once("./controllers/studentsController.php");
+/*Este archivo se encarga de manejar las rutas específicas del
+módulo students. Actúa como un "puente" entre la solicitud que
+llega del frontend y el controlador que se encarga de
+procesarla.*/
+
+//Cargar y ejecutar archivos necesarios
+require_once("./config/databaseConfig.php");//contiene los datos para conectarse a la base
+require_once("./routes/routesFactory.php");//contiene la funcion routerequest
+require_once("./controllers/studentsController.php");//contiene funciones handle
 
 // routeRequest($conn);
 
@@ -21,17 +27,19 @@ require_once("./controllers/studentsController.php");
  * para casos particulares
  * o validaciones:
  */
-routeRequest($conn, [
-    'POST' => function($conn) 
+
+//Extensión personalizada de la ruta POST
+routeRequest($conn, [ //usa routerequest para definir ruta personalizada para solicitudes POST
+    'POST' => function($conn) //Define lo que debe pasar si el metodo HTTP es Post
     {
         // Validación o lógica extendida
-        $input = json_decode(file_get_contents("php://input"), true);
-        if (empty($input['fullname'])) 
+        $input = json_decode(file_get_contents("php://input"), true);//Lee el cuerpo del mensaje http en json, lo convierte en un array de PHP.
+        if (empty($input['fullname'])) //si el campo fullname no esta rellenado
         {
-            http_response_code(400);
+            http_response_code(400);//error
             echo json_encode(["error" => "Falta el nombre"]);
             return;
         }
-        handlePost($conn);
+        handlePost($conn);//si esta todo bien. se guarda el estudiante en la base de datos.
     }
 ]);
