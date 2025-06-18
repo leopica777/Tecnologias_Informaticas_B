0b7c49dd445c1aaf39008b371504afcc8118e5f1
leopica <85765754+leopica777@users.noreply.github.com>
2025-06-17 21:37:52 -0300

    Update students.php

--- Refactorizar 3/backend/models/students.php
+++ Refactorizar 3/backend/models/students.php
@@ -9,60 +9,67 @@
 *    Iteration   : 3.0 ( prototype )
 */
 
-function getAllStudents($conn) 
+/*
+Este archivo define una serie de funciones PHP que ejecutan
+consultas SQL sobre la tabla students. Cada función realiza
+una acción típica de un CRUD
+*/
+
+function getAllStudents($conn) //obtener todos los estudiantes
 {
-    $sql = "SELECT * FROM students";
+    $sql = "SELECT * FROM students"; //Define una consulta SQL que selecciona todas las filas de la tabla students.
+
 
     //MYSQLI_ASSOC devuelve un array ya listo para convertir en JSON:
-    return $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
+    return $conn->query($sql)->fetch_all(MYSQLI_ASSOC); //ejecuta la consulta sql. fetchall convierte en array asociativo. 
 }
 
-function getStudentById($conn, $id) 
+function getStudentById($conn, $id) //obtener estudiante por id
 {
-    $stmt = $conn->prepare("SELECT * FROM students WHERE id = ?");
-    $stmt->bind_param("i", $id);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    $stmt = $conn->prepare("SELECT * FROM students WHERE id = ?"); //prepara consulta sql con ? como marcador de posicion
+    $stmt->bind_param("i", $id); //remplaza el ? por un entero i
+    $stmt->execute(); //ejecuta la consulta
+    $result = $stmt->get_result();//obtiene el resultado
 
     //fetch_assoc() devuelve un array asociativo ya listo para convertir en JSON de una fila:
-    return $result->fetch_assoc(); 
+    return $result->fetch_assoc(); //extrae UNA SOLA fila como array asociativo
 }
 
-function createStudent($conn, $fullname, $email, $age) 
+function createStudent($conn, $fullname, $email, $age) //CREAR ESTUDIANTES
 {
-    $sql = "INSERT INTO students (fullname, email, age) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO students (fullname, email, age) VALUES (?, ?, ?)";//Prepara consulta INSERT para agregar un nuevo estudiante
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ssi", $fullname, $email, $age);
-    $stmt->execute();
+    $stmt->execute();//ejecuta
 
     //Se retorna un arreglo con la cantidad e filas insertadas 
     //y id insertado para validar en el controlador:
-    return 
+    return //devuelve un array
     [
-        'inserted' => $stmt->affected_rows,        
-        'id' => $conn->insert_id
+        'inserted' => $stmt->affected_rows,  //cuantas filas se insertaron      
+        'id' => $conn->insert_id //id generado por la BD. Es para validar
     ];
 }
 
-function updateStudent($conn, $id, $fullname, $email, $age) 
+function updateStudent($conn, $id, $fullname, $email, $age) //Actualizar estudiante
 {
     $sql = "UPDATE students SET fullname = ?, email = ?, age = ? WHERE id = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssii", $fullname, $email, $age, $id);
-    $stmt->execute();
+    $stmt->bind_param("ssii", $fullname, $email, $age, $id); //ssii = string, string, integer,integer
+    $stmt->execute();//ejecuta
 
     //Se retorna fila afectadas para validar en controlador:
-    return ['updated' => $stmt->affected_rows];
+    return ['updated' => $stmt->affected_rows]; //Devuelve cuántas filas fueron modificadas
 }
 
-function deleteStudent($conn, $id) 
+function deleteStudent($conn, $id) //borrar estudiante
 {
-    $sql = "DELETE FROM students WHERE id = ?";
+    $sql = "DELETE FROM students WHERE id = ?";//Prepara consulta DELETE por id
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $id);
-    $stmt->execute();
+    $stmt->bind_param("i", $id); //EVITA INYECCIONES DE SQL
+    $stmt->execute();//ejecuta
 
     //Se retorna fila afectadas para validar en controlador
-    return ['deleted' => $stmt->affected_rows];
+    return ['deleted' => $stmt->affected_rows]; //Devuelve cuántas filas fueron modificadas
 }
 ?>
