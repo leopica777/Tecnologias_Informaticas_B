9f110323438fb047377f2199667c7449a48e8d27
leopica <85765754+leopica777@users.noreply.github.com>
2025-06-17 22:18:23 -0300

    Update studentsSubjects.php

--- Refactorizar 3/backend/models/studentsSubjects.php
+++ Refactorizar 3/backend/models/studentsSubjects.php
@@ -9,22 +9,22 @@
 *    Iteration   : 3.0 ( prototype )
 */
 
-function assignSubjectToStudent($conn, $student_id, $subject_id, $approved) 
+function assignSubjectToStudent($conn, $student_id, $subject_id, $approved) //Asigna materia a estudiante
 {
-    $sql = "INSERT INTO students_subjects (student_id, subject_id, approved) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO students_subjects (student_id, subject_id, approved) VALUES (?, ?, ?)"; //insert con placeholders ?? para evitar inyecciones sql
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("iii", $student_id, $subject_id, $approved);
+    $stmt->bind_param("iii", $student_id, $subject_id, $approved);//se enlanza los valores de los placeholders
     $stmt->execute();
 
     return 
-    [
+    [//devuelve cantidad de filas insertadas y el id nuevo registro
         'inserted' => $stmt->affected_rows,        
         'id' => $conn->insert_id
     ];
 }
 
 //Query escrita sin ALIAS resumidos (a mi me gusta más):
-function getAllSubjectsStudents($conn) 
+function getAllSubjectsStudents($conn) ///recupera todas las relaciones estudiante materia. junto con nombres completos de estudiante y materia gracias al JOIN
 {
     $sql = "SELECT students_subjects.id,
                 students_subjects.student_id,
@@ -36,11 +36,11 @@ function getAllSubjectsStudents($conn)
             JOIN subjects ON students_subjects.subject_id = subjects.id
             JOIN students ON students_subjects.student_id = students.id";
 
-    return $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
+    return $conn->query($sql)->fetch_all(MYSQLI_ASSOC);//Devuelve array asociativo multidimensional 
 }
 
 //Query escrita con ALIAS resumidos:
-function getSubjectsByStudent($conn, $student_id) 
+function getSubjectsByStudent($conn, $student_id) //no usada
 {
     $sql = "SELECT ss.subject_id, s.name, ss.approved
         FROM students_subjects ss
@@ -58,15 +58,15 @@ function updateStudentSubject($conn, $id, $student_id, $subject_id, $approved)
 {
     $sql = "UPDATE students_subjects 
             SET student_id = ?, subject_id = ?, approved = ? 
-            WHERE id = ?";
+            WHERE id = ?"; //actualiza una relacion existente con nuevos valores
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("iiii", $student_id, $subject_id, $approved, $id);
     $stmt->execute();
 
-    return ['updated' => $stmt->affected_rows];
+    return ['updated' => $stmt->affected_rows];//devuelve cuantas filas fueron modificadas
 }
 
-function removeStudentSubject($conn, $id) 
+function removeStudentSubject($conn, $id) //Elimina la relación estudiante-materia identificada por id.
 {
     $sql = "DELETE FROM students_subjects WHERE id = ?";
     $stmt = $conn->prepare($sql);
